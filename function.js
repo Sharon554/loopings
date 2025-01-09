2a0bff62211604336987aa73dd5787d03b981fea
Sharon554 <2211CS010554@mallareddyuniversity.ac.in>
2025-01-09 13:56:51 +0530

    sending message to git

---
+++ function.js
@@ -0,0 +1,49 @@
+// !function without parameters
+//function demo(){
+    //console.log("function without parameters");
+    //}
+    //demo();
+    //function addToCart(){
+    //}
+    //demo();
+    //!function with parameters
+    //function userDetails(name, age ,city){
+    //console.log(name);
+    //console.log(age);
+    //console.log(city);
+    //}
+    //userDetails("Sharon",20."Hyderabad");
+    
+    //! anonymus function
+    //function(){
+        //console.log("Anonymus function");
+    //}
+    //();
+     
+    //!function expression
+    //let x = function(){
+       // console.log?("function exprerssion is executing");
+    //}
+    //x();
+
+    //!IIFE
+    //(function(){
+        //console.log("IIFE");
+    //})();
+    
+    //! arrow function
+    //function demo(){
+
+    //}
+    //console.log("Hello");
+//}
+//demo();
+ //let x =()=>console.log("Arrow function");
+ //x();
+ //let x=(a , b)=>console.log(a+b);
+ //x(5,5); 
+   
+ //let x=_=>{console.log("hi")
+ //console.log("Hello");
+ //console.log("bye");}
+ //x();
