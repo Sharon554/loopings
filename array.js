a2be579e93fe548fec1132bd7674a528ad3c26a5
Sharon554 <2211CS010554@mallareddyuniversity.ac.in>
2025-01-20 20:08:13 +0530

    sending files

---
+++ array.js
@@ -0,0 +1,38 @@
+//let user1 = "Ramya";
+//let user2 = "Sai Nikhil";
+//let user3 = "SaiSmitha";
+//let user4 = "Rahul";
+//let user5 = "Prabhas";
+
+//console.log(user1);
+//console.log(user2);
+//console.log(user3);
+//console.log(user4);
+//console.log(user5);
+
+//let users = ["Ramya" , "Sai Nikhil" , "SaiSmitha" , "Rahul" , "Prabhas" , "NTR"];
+// let x = users.map(user)=>{
+    
+// return user; //!["Ramya" , " Sai Nikhil" , "SaiSmitha" , "Rahul" , "Prabhas" , "NTR"]
+//});
+// console.log(x);
+ //let x = users.forEach((user)=>{
+    // console.log(user);
+    //return user;
+ //});
+ //console.log(x); // !underfined
+
+ //! JSON
+
+ //let userDetails ={
+    //name:"Sam",
+    //age:25,
+    //city:"Hyd"
+ //};
+ //console.log(userDetails);
+
+ //let x = JSON.stringify(userDetails);
+ //console.log("JSON object"+x);
+ 
+ //let y = JSON.parse(x);
+ //console.log?(y);
