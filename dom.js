1edc2fbd1575d8d5219f2eb2d066c1d7adb0fc0f
Sharon554 <2211CS010554@mallareddyuniversity.ac.in>
2025-01-23 20:20:04 +0530

    sending message

---
+++ dom.js
@@ -0,0 +1,11 @@
+console.log(window);
+console.log(document);
+
+let element = document.getElementById("demo");
+element.innerText = Google
+console.log(element);
+
+let ele = document.getElementsByName("test");
+ele.innerText= "<h1>Header</h1>"
+ele.innerHTML = "<h1Header<h1>"
+console.log(ele);
