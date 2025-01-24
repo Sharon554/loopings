99be1ed762bbfc38a054c82e767d587c9ff8f6d3
Sharon554 <2211CS010554@mallareddyuniversity.ac.in>
2025-01-24 22:13:10 +0530

    sending messages

--- dom.js
+++ dom.js
@@ -1,11 +1,62 @@
-console.log(window);
-console.log(document);
+//console.log(window);
+//console.log(document);
 
-let element = document.getElementById("demo");
-element.innerText = Google
-console.log(element);
+//let element = document.getElementById("demo");
+//element.innerText = Google
+//console.log(element);
 
-let ele = document.getElementsByName("test");
-ele.innerText= "<h1>Header</h1>"
-ele.innerHTML = "<h1Header<h1>"
-console.log(ele);
+//let ele = document.getElementsByName("test");
+//ele.innerText= "<h1>Header</h1>"
+//ele.innerHTML = "<h1Header<h1>"
+//console.log(ele);
+
+//!
+//let ele = document.getElementsByClassName("test");
+//console.log(ele);
+//console.log(Array.isArray(ele));
+//ele[0].innerText="Hello"
+//ele[0].style.backgroungColor="tomato"
+
+//!spread operator : it will take out each and every value from the original array
+//! and store it inside one more new array(pure array).
+//? syntax :- [....variable];
+
+//let x = [...ele];
+//console.log(x ,Array.isArray(x));
+//x.map(element=>{
+    //console.log(element);
+    //element.style.backgroundColor="orange"
+//})
+
+//!
+ //let ele = document.getElementsByTagName("div");
+ //console.log(ele);
+
+//let ele = document.getElementsByName("formData");
+//console.log(ele);
+//[...ele].map(inp=>{
+    //console.log(inp.value);
+
+//});
+//let ele = document.querySelector("mru");
+//console.log(ele);
+
+//let ele = document.querySelector(".test");
+//console.log(ele);
+
+//let ele = document.querySelectorAll(".test");
+//console.log(ele);
+
+//let btn = document.querySelector("#btn");
+//! addEventListner("event" ,()=>{})
+    //btn.addEventListener("click",()=>{
+        //console.log("button clicked");
+        //alert("hi")
+        //document.body.style.backgroundColor="tomato"
+
+    //})
+
+    //let btn = document.querySelector("#btn");
+    //btn.addEventListener("dbclick",()=>{
+        //console.log("double clicked");
+    //})
