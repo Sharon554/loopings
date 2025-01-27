27ad2017666ed809618d8f98b9df0e5c8e7db171
Sharon554 <2211CS010554@mallareddyuniversity.ac.in>
2025-01-27 21:07:29 +0530

    sending files

--- dom.js
+++ dom.js
@@ -90,21 +90,92 @@
     //document.body.appendChild(ele);
    // document.body.appendChild(ele);
 
-    let form = document.querySelector("form");
-    let username = document.getElementById("uName");
-    let mail =  document.getElementById("uName");
-    let psw = document.getElementById("uPass");
+    //let form = document.querySelector("form");
+    //let username = document.getElementById("uName");
+    //let mail =  document.getElementById("uName");
+    //let psw = document.getElementById("uPass");
     
-    form.addEventListener("submit",(event)=>{
-        event.preventDefault();
+    //form.addEventListener("submit",(event)=>{
+        //event.preventDefault();
         //console.log(event);
         //console.log("form submitted");
-        let uname = username.value;
-        let uemail = mail.value;
-        let upass = psw.value;
-        let userDetails={ 
-            uname, uemail, upass
-        }
-        console.log(userDetails);
-        localStorage.setItem("userData", JSON.stringify(userDetails))
+        //let uname = username.value;
+        //let uemail = mail.value;
+        //let upass = psw.value;
+        //let userDetails={ 
+            //uname, uemail, upass
+        //}
+        //console.log(userDetails);
+        //localStorage.setItem("userData", JSON.stringify(userDetails))
+    //})
+
+
+    //!
+    
+    //let vd = document.createElement("video");
+    //video.setAttribute(s"src","./video123.mp4")
+    //console.log(video);
+    //})
+
+
+    //documenty.body.appendChild(vd)
+
+    //let h1 = document.createdElement("h1");
+    //h1.setAttribute("id","demo")
+    //h1.addEventListener
+
+
+    let mainEle = document.createElement("div");
+    mainEle.setAttribute("id","mainBlock");
+    console.log(mainEle);
+    
+    let topEle = document.createElement("div");
+    topEle.setAttribute("class","topBlock");
+    console.log(topEle);
+
+    let bottomEle = document.createElement("div");
+    bottomEle.setAttribute("class","bottomBlock");
+    console.log(bottomEle);
+
+
+
+    let image = document.createElement("img");
+    image.src="https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg?auto=compress&cs=tinysrgb&w=600";
+    image.style.width="550px";
+
+
+    mainEle.style.border="2px solid red";
+    mainEle.style.width="550px";
+    mainEle.style.height="550px";
+
+    console.log(mainEle);
+
+    let btn = document.createElement("button");
+    btn.innerText="View More";
+    btn.style.border="none";
+    btn.style.padding="20px";
+    btn.style.backgroundColor="dodgerblue";
+    btn.style.color="white";
+
+    //bottomEle.appendChild(h1);
+    bottomEle.appendChild(btn);
+
+    topEle.appendChild(image);
+    mainEle.appendChild(topEle);
+    mainEle.appendChild(bottomEle);
+    document.body.appendChild(mainEle);
+
+
+    let form = document.querySelector("form");
+
+    let username = document.getElementById("uName");
+    let password = document.getElementById("uPass");
+
+    let gender = document.getElementsByName("gender");
+    //console.log(gender);
+
+    form.addEventListener("submit" , event=>{
+        event.preventDefault();
     })
+
+    
